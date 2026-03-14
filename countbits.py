6016ee8a617b97ed12446a164744c1fd7479cd20
samizzz67 <abiyafikre@gmail.com>
2026-03-14 20:45:35 +0300

    add occurence of 1 range of numbers in bits

--- countbits.py
+++ countbits.py
@@ -1,6 +1,13 @@
-n=5
-index=[]
+class Solution(object):
+    def countBits(self, n):
+        """
+        :type n: int
+        :rtype: List[int]
+        """
+        
+        index=[]
 
-for i in range(n+1):
-    index.append(bin(i).count('1'))
-print(index)
+        for i in range(n+1):
+            index.append(bin(i).count('1'))
+        return (index)
+        
